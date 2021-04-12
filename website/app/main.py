34ec910fa10e414ebbb5e7432d8e5be88ed72355
Dominic Marques <dominicmarques21@gmail.com>
2021-04-11 22:06:09 -0400

    Add templating for response, and a place for processing

--- website/app/main.py
+++ website/app/main.py
@@ -1,8 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 tasks = {1:{"task_name": "do stuff", "status":False}}
 ntasks = 1
@@ -13,6 +15,10 @@ async def main():
         content = f.read()
         return HTMLResponse(content=content)
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+@app.post("/uploadfile/", response_class=HTMLResponse)
+async def create_upload_file(request: Request, file: UploadFile = File(...)):
+    # Process results here, place in results var
+    # Should use async if at all possible, following
+    # https://fastapi.tiangolo.com/async/
+    results = {"test": "Hello, world!"}
+    return templates.TemplateResponse("results.html", {"request":request, "results": results})
