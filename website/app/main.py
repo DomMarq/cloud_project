84e2826537b09233f9b7f5a52e39dba4516f7e34
Dominic Marques <dominicmarques21@gmail.com>
2021-04-13 16:08:21 -0400

    Add link to aws deploy guide

--- website/app/main.py
+++ website/app/main.py
@@ -24,3 +24,4 @@ async def create_upload_file(request: Request, file: UploadFile = File(...)):
 
 # docker build -t fastapi:latest .
 # docker run -p 9372:9372 fastapi:latest
+# https://medium.com/@meetakoti.kirankumar/deploying-fastapi-web-application-in-aws-a1995675087d
