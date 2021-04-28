b38e4e62f910a84e893b5f574c4fb000b988e554
Dominic Marques <dominicmarques21@gmail.com>
2021-04-28 16:41:47 -0400

    Website deployment on lambda now has basic functionality

--- website2/app/main.py
+++ website2/app/main.py
@@ -2,28 +2,24 @@ from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from mangum import Mangum
+from v1.routers import router
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.include_router(router, prefix="/v1")
 
-tasks = {1:{"task_name": "do stuff", "status":False}}
-ntasks = 1
 
 @app.get("/")
 async def main():
     with open("index.html", "r") as f:
         content = f.read()
+        print("get recieved")
         return HTMLResponse(content=content)
 
-@app.post("/uploadfile/", response_class=HTMLResponse)
-async def create_upload_file(request: Request, file: UploadFile = File(...)):
-    # Process results here, place in results var
-    # Should use async if at all possible, following
-    # https://fastapi.tiangolo.com/async/
-    results = {"filename": file.filename}
-    return templates.TemplateResponse("results.html", {"request":request, "results": results})
 
 # docker build -t fastapi:latest .
 # docker run -p 9372:9372 fastapi:latest
 # https://medium.com/@meetakoti.kirankumar/deploying-fastapi-web-application-in-aws-a1995675087d
 handler = Mangum(app=app)
+
+# curl -i -X POST -H "Content-Type: multipart/form-data" -F "file=@package.sh" https://kx0v87byqj.execute-api.us-east-1.amazonaws.com/dev/v1/uploadfile
