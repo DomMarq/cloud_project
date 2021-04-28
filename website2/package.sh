6a40f526ec2fbe328caf83d726cb505ebcd5af26
Dominic Marques <dominicmarques21@gmail.com>
2021-04-28 15:26:25 -0400

    add packaging script

---
+++ website2/package.sh
@@ -0,0 +1,5 @@
+rm lambda.zip
+root_dir=$(pwd)
+venv_dir="$root_dir/venv/lib/python3.8/site-packages"
+cd $venv_dir && zip -r9 "$root_dir/lambda.zip" . \
+&& cd "$root_dir/app" && zip -g ../lambda.zip -r .
