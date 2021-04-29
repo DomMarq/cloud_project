18325aaf6952968c9a605108b82419a55125c76b
Dominic Marques <dominicmarques21@gmail.com>
2021-04-29 19:31:48 -0400

    Fix set_up_venv.sh, re-add tensorflow .whl

--- website2/set_up_venv.sh
+++ website2/set_up_venv.sh
@@ -1,5 +1,6 @@
 # run this before starting up the website or deploying
 rm -r venv/
 python3 -m venv venv
-DOCKER_BUILDKIT=1 docker build --output layers/tflite_runtime/ .
+DOCKER_BUILDKIT=1 docker build --output . .
 pip install -r requirements.txt
+rm tflite_runtime-2.4.0-py3-none-any.whl
