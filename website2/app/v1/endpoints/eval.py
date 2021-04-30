0a784441188de9d22eb33f9ca385c85f862e7bda
Dominic Marques <dominicmarques21@gmail.com>
2021-04-30 15:49:19 -0400

    Fix base64 encoded bug on lambda

--- website2/app/v1/endpoints/eval.py
+++ website2/app/v1/endpoints/eval.py
@@ -1,28 +1,28 @@
-import os
+import base64
 from fastapi import APIRouter, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import run_model_lite
-from typing import List
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
 @router.post("/uploadfile", response_class=HTMLResponse)
-async def create_upload_file(request: Request, file: List[UploadFile] = File(...)):
+async def create_upload_file(request: Request, file: UploadFile = File(...)):
     # Process results here, place in results var
     # Should use async if at all possible, following
     # https://fastapi.tiangolo.com/async/
     # filename = file.filename
     # await file.close()
     # f.close()
-    file = file[0]
     print("file recieved: ", file.filename)
     print("content type: ", file.content_type)
     bytes = await file.read()
+    if len(bytes) > 500000:
+        bytes = base64.decode(bytes)
     print("File length: ", len(bytes))
-    result = run_model_lite.predict_image(bytes[-369053:])
-    results = {"filename": "asdf", "result": result}
+    result = run_model_lite.predict_image(bytes)
+    results = {"filename": file.filename, "result": result}
     await file.close()
     return templates.TemplateResponse("results.html", {"request":request, "results": results})
