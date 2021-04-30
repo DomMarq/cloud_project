875c656aa84c539183f8cccdd85fd96d723540d9
Dominic Marques <dominicmarques21@gmail.com>
2021-04-30 02:39:37 -0400

    Add lite model execution - not working on lambda

--- website2/app/v1/endpoints/eval.py
+++ website2/app/v1/endpoints/eval.py
@@ -2,23 +2,27 @@ import os
 from fastapi import APIRouter, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import run_model_lite
+from typing import List
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
 @router.post("/uploadfile", response_class=HTMLResponse)
-async def create_upload_file(request: Request, file: UploadFile = File(...)):
+async def create_upload_file(request: Request, file: List[UploadFile] = File(...)):
     # Process results here, place in results var
     # Should use async if at all possible, following
     # https://fastapi.tiangolo.com/async/
-    f = open("images/image.png", "wb+")
-    f.write(file.file.read())
-    filename = file.filename
-    file.close()
-    f.close()
-    result = os.popen("python3 run_model.py images/image.png")
-    os.remove("images/image.png")
+    # filename = file.filename
+    # await file.close()
+    # f.close()
+    file = file[0]
     print("file recieved: ", file.filename)
-    results = {"filename": file.filename, "result": result.read()}
+    print("content type: ", file.content_type)
+    bytes = await file.read()
+    print("File length: ", len(bytes))
+    result = run_model_lite.predict_image(bytes[-369053:])
+    results = {"filename": "asdf", "result": result}
+    await file.close()
     return templates.TemplateResponse("results.html", {"request":request, "results": results})
