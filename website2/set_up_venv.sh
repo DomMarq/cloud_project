87344e8c10fb940a9b33ea7de02c28a01a843cee
Dominic Marques <dominicmarques21@gmail.com>
2021-04-30 00:17:48 -0400

    Add additional clarification

--- website2/set_up_venv.sh
+++ website2/set_up_venv.sh
@@ -1,6 +1,8 @@
 # run this before starting up the website or deploying
 rm -r venv/
 python3 -m venv venv
+# this command requires docker to be installed and running - docker desktop for local execution
 DOCKER_BUILDKIT=1 docker build --output . .
-pip install -r requirements.txt
-rm tflite_runtime-2.4.0-py3-none-any.whl
+# this line will probably not execute properly - run it in terminal
+source venv/bin/activate && pip install -r requirements.txt \
+&& rm tflite_runtime-2.4.0-py3-none-any.whl
