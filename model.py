ba486cfa899e2388dd2db561389d7240294e9d5e
bhart5nd <62891517+bhart5nd@users.noreply.github.com>
2021-05-11 11:02:55 -0400

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ from keras.layers import Flatten
 import time as timer
 
 # start timing 
-start_time = timer.time()
+start_train_time = timer.time()
 
 # for xray data
 data_dir = 'data/xrays/'
@@ -165,21 +165,34 @@ history = model.fit(
     callbacks=cp_callback
 )
 
-# save model
-model.save('saved_model/my_model')
+# end time
+end_train_time = timer.time()
+train_time = end_train_time - start_train_time
+train_day = train_time // (24 * 3600)
+train_time = train_time % (24 * 3600)
+train_hour = train_time // 3600
+train_time %= 3600
+train_minutes = train_time // 60
+train_time %= 60
+train_seconds = np.round(train_time,0)
+print(f"Total model execution time: {train_day} days, {train_hour} hours, {train_minutes} minutes, {train_seconds} seconds")
 
 # model evaluation
+start_eval_time = timer.time()
 _, test_auc = model.evaluate(test_dataset, verbose=0)
 print('Test auc:', test_auc)
 
-# end time
-end_time = timer.time()
-time = end_time - start_time
-day = time // (24 * 3600)
-time = time % (24 * 3600)
-hour = time // 3600
-time %= 3600
-minutes = time // 60
-time %= 60
-seconds = np.round(time,0)
-print(f"Total code execution time: {day} days, {hour} hours, {minutes} minutes, {seconds} seconds")
+end_eval_time = timer.time()
+eval_time = end_eval_time - start_eval_time
+eval_day = eval_time // (24 * 3600)
+eval_time = eval_time % (24 * 3600)
+eval_hour = eval_time // 3600
+eval_time %= 3600
+eval_minutes = eval_time // 60
+eval_time %= 60
+eval_seconds = np.round(eval_time,0)
+print(f"Total evaluation execution time: {eval_day} days, {eval_hour} hours, {eval_minutes} minutes, {eval_seconds} seconds")
+
+
+# save model
+model.save('saved_model/my_model_10')
